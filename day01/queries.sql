2ae37d40e196ba9293e4e0a7e725cf5dbf3b47da
hossein-sadri <90840692+hossein-sadri@users.noreply.github.com>
2025-12-15 19:04:07 +0330

    Create queries.sql

---
+++ day01/queries.sql
@@ -0,0 +1,10 @@
+-- Day 1: Basic SELECT queries
+-- SELECT is used to retrieve columns from a table.
+
+-- Select specific columns
+SELECT title, year
+FROM movies;
+
+-- Select all columns
+SELECT *
+FROM movies;
